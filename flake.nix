a216a33aa45cfbba3c12f688ed88921b00b862d3
Mori2003 <github.mk@keszler.info>
2024-12-03 14:50:47 +0100

    Format flake.nix and add systems

--- flake.nix
+++ flake.nix
@@ -1,28 +1,41 @@
 {
-  description = "FHS environment for python";
+  description = "FHS environment for python with uv";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { nixpkgs, ... } @ inputs:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-    # Set up a FHS development shell using standard packages based on appimageTools package.
-    devShells.${system}.default = (pkgs.buildFHSEnv (pkgs.appimageTools.defaultFhsEnvArgs // {
-          name = "python-dev-env";
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+        ] (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      # Set up a FHS development shell using standard packages based on appimageTools package.
+      devShells = forAllSystems (pkgs: {
+        default =
+          (pkgs.buildFHSEnv (
+            pkgs.appimageTools.defaultFhsEnvArgs
+            // {
+              name = "python-dev-env";
 
-          # Packages installed in the development shell
-          targetPkgs = pkgs: with pkgs; [
-
-            python3
-            python3Packages.uv
-          ];
-          # Commands to be executed in the development shell
-          runScript = "echo \n\"entering nix-shell...\"";
-        })).env;
-  };
+              # Packages installed in the development shell
+              targetPkgs =
+                pkgs: with pkgs; [
+                  python3
+                  python3Packages.uv
+                ];
+              # Commands to be executed in the development shell
+              profile = ''
+                echo -e "\e[1;34mentering dev-shell...\e[0m"
+              '';
+            }
+          )).env;
+      });
+    };
 }
